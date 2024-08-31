c4754f505b4edf896d6848f7e6f7880d7f45db6f
ThatWeirdLady <ThatWeirdLady@gmail.com>
2024-08-31 10:44:19 -0400

    begin message_service

--- subscribe_service/src/main/java/example/SubscribeService.java
+++ subscribe_service/src/main/java/example/SubscribeService.java
@@ -2,7 +2,6 @@ package example;
 
 import java.util.Collections;
 import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpHeaders;
